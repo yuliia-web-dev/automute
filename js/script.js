49fc0baa368dc0323b6ecf8e307842a087eaa2eb
yuliia-web-dev <yuliia.devweb@gmail.com>
2025-03-12 15:43:23 +0200

    some changes

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 "use strict"
 
+document.addEventListener('DOMContentLoaded', function () {
+	const closeBtn = document.querySelector('.announcement__btn');
+	const announcement = document.querySelector('.announcement');
+
+	if (!closeBtn || !announcement) return;
+
+	closeBtn.addEventListener('click', function () {
+		announcement.classList.add('hidden');
+	});
+});
